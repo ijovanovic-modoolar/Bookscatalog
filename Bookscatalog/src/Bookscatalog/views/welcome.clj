72401bfcbcbdeac6b7e6a3689ee2f772c2e36c47
netbox <netbox@netbox-VirtualBox>
2014-04-19 13:41:32 +0200

    Removed Noir welcome view to utilize "/"

--- Bookscatalog/src/Bookscatalog/views/welcome.clj
+++
@@ -1,8 +0,0 @@
-(ns Bookscatalog.views.welcome
-  (:require [Bookscatalog.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to Bookscatalog"]))
